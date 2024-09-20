d8b38a5c8ae0b0bae344a0df0cde64d8e44f1208
FabianoLp <fabianomontaro@gmail.com>
2024-09-20 19:23:21 -0300

    Foi feito o Exercicio 04

---
+++ exercicio03/exercicio03.py
@@ -0,0 +1,20 @@
+# Par A
+A1 = (4/2) + (2/4)
+A2 = 4/2 + 2/4
+
+# Par B
+B1 = 4 / (2+2) / 4
+B2 = 4 / 2 + 2 / 4
+
+# Par C
+C1 = (4+2) * 2 - 4
+C2 = 4 + 2 * 2 - 4
+
+# Resultados
+print("A1:", A1, "A2:", A2, "Iguais:", A1 == A2)
+print("B1:", B1, "B2:", B2, "Iguais:", B1 == B2)
+print("C1:", C1, "C2:", C2, "Iguais:", C1 == C2)
+
+#Par A: (4/2) + (2/4) e 4/2 + 2/4 produzem o mesmo resultado.
+#Par B: 4 / (2+2) / 4 e 4 / 2 + 2 / 4 não produzem o mesmo resultado.
+#Par C: (4+2) * 2 - 4 e 4 + 2 * 2 - 4 não produzem o mesmo resultado.
